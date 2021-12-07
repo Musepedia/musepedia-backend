01de48daba2cfd840ffa07b3bca6fc5b3c268d21
Uzemiu <36361568+Uzemiu@users.noreply.github.com>
2021-12-07 14:58:16 +0800

    Revert "改进登录"
    This reverts commit ad844ab75062e897731446a32b8b88f62dbfe118.

--- mgsbackend-core/src/main/java/cn/abstractmgs/core/model/dto/UserDTO.java
+++ mgsbackend-core/src/main/java/cn/abstractmgs/core/model/dto/UserDTO.java
@@ -1,14 +1,4 @@
 package cn.abstractmgs.core.model.dto;
 
-import com.baomidou.mybatisplus.annotation.TableField;
-import lombok.Data;
-
-@Data
 public class UserDTO {
-
-    private Long id;
-
-    private String nickname;
-
-    private String avatarUrl;
 }

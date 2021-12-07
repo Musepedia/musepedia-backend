7683c8c5c35066a8fee7f592c95437b3749bade8
Uzemiu <36361568+Uzemiu@users.noreply.github.com>
2021-12-07 14:58:16 +0800

    Revert "写了一半建表语句"
    This reverts commit 9e17152ea9c481150b8d6c9514488e7ad5a9d400.

--- mgsbackend-core/src/test/java/request/WxAuth.java
+++ mgsbackend-core/src/test/java/request/WxAuth.java
@@ -1,36 +1,22 @@
 package request;
 
-import cn.abstractmgs.core.App;
-import cn.abstractmgs.core.model.dto.Code2SessionResponse;
-import cn.abstractmgs.core.utils.CipherUtil;
+import cn.abstractmgs.core.model.dto.WxUser;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.Map;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = App.class)
 public class WxAuth {
-
-    @Resource
-    RestTemplate restTemplate;
 
     @Test
     public void test(){
-        String iv = "N1t3uTDl3I3xRNCn/LM7bg==";
-        String encryptedData = "lrDmQD4uzrm4rpTh0GT8GbmOfLQC06l1lGE0IjsCzd8QeNuEXR2juYHz90kpidDv+CloD7NytAQYAk5WobWAQhOvk5GXMEtvXZmgkFnhzrUsM1iCAVCo/l6SZPMbZvQNTwh8zjKxp4hbwUa/O+s9a/I2t1bjGTmF/vE0q0L/+faVM3P72V2EFCeNquZWln9Nc+mxEmsmHVxJUHA1hKjxHU8jEu76imGSRQC1F9a8bMWUMXreubIuAetHFxmGzYtm5HBVaExgzrxsMfbALSc1XdJz2TpTzM8Pe7J+13iz0MAGBkxY/dDLz0sAliwy7wLbJG0dO0i1IvKfszFTZx9Ml6ww/DmQPtU/kBsVD8KHZseMGfvqm7rIbgrzD39+2QSxv1Bk9As1X96DOV14V/3hqA==";
-        String url = "https://api.weixin.qq.com/sns/jscode2session?" +
-                "appid=wxfe9a49b9f26c289d" +
-                "&secret=60232f676e1215a24a8e357d2b1c76dc" +
-                "&js_code=" + "003POH000kdWRM12lH2002OZyS0POH0H" +
-                "&grant_type=authorization_code";
-        Code2SessionResponse resp = restTemplate.getForObject(url , Code2SessionResponse.class, new HashMap<>());
-        System.out.println(resp.getOpenid());
-        String phoneNumber = CipherUtil.decryptPhoneNumber(resp.getSession_key(), encryptedData, iv);
-
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, String> param = new HashMap<>();
+        param.put("appid", "wxfe9a49b9f26c289d");
+        param.put("secret", "60232f676e1215a24a8e357d2b1c76dc");
+        param.put("js_code", "073prI10057pQM1WOY300aELhD4prI1M");
+        param.put("grant_type", "authorization_code");
+        WxUser wxUser = restTemplate.getForObject("https://api.weixin.qq.com/sns/jscode2session", WxUser.class, param);
     }
 }

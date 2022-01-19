b313f4158f9e8588a81bbfdf667da08aadf10a5f
Sornk <598839297@qq.com>
2022-01-19 13:54:48 +0800

    0.2.4 mutiple random exhibits supported

--- mgsbackend-core/src/main/java/cn/abstractmgs/core/service/ExhibitService.java
+++ mgsbackend-core/src/main/java/cn/abstractmgs/core/service/ExhibitService.java
@@ -2,6 +2,7 @@ package cn.abstractmgs.core.service;
 
 import cn.abstractmgs.core.model.entity.Exhibit;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,6 +11,10 @@ public interface ExhibitService extends IService<Exhibit> {
 
     Exhibit getExhibitInfoById(Long id);
 
-    List<Exhibit> getRandomExhibits(int limit);
+    List<String> selectRandomExhibitId(@Param("limit") int limitPerExhibitionHall);
+
+    List<Exhibit> selectRandomExhibits(@Param("ids") List<Integer> ids);
+
+    List<Exhibit> getRandomExhibits(int limitPerExhibitionHall);
 
 }
